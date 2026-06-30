221babe3cb887eec07c5de0c1155d786aa16e001
Lomito <usuario@Lomitos-MacBook-Air.local>
2026-06-30 10:57:49 -0500

    modificacion de logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,7 +6,7 @@ export default function Navbar() {
     <nav className="navbar">
       <div className="container navbar-content">
         <Link to="/inicio" className="navbar-logo">
-          <img src="/logo-en-blanco.png" alt="Satori" />
+          <img src="/logo-inicio.PNG" alt="Satori" />
         </Link>
         <ul className="navbar-menu">
           <li><Link to="/inicio">Inicio</Link></li>
